5ccfb289b16712ba84ea6bb932a3d044bc8279f8
kwro <64830762+kwro@users.noreply.github.com>
2020-05-05 11:03:02 +0200

    Delete split_dataset.py

--- split_dataset.py
+++
@@ -1,6 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-X_train, X_valid, y_train, y_valid = train_test_split(data.iloc[:, 1:], data.Y,
-                                                     test_size=0.3,
-                                                     random_state=1)
-
